0803d5dc3e79fe99b414e47e71e7575693253dd2
merrickj <jmerrick@alumni.stanford.edu>
2022-08-17 12:04:54 +0100

    simple JuMP MIP test

---
+++ examples/jump/simple_mip/simple_mip_test.jl
@@ -0,0 +1,33 @@
+import DotEnv
+DotEnv.config()
+
+using MOSInterface
+
+@testset "Simple_MIP" begin
+    # Interface
+    interface = Interface()
+
+    # Delete model
+    delete_model_with_name(interface, "Simple MIP JuMP Model")
+
+    # New model
+    model = new_model(interface, "./examples/jump/simple_mip/simple_mip.jl")
+
+    # Get model
+    model = get_model_with_name(interface, "Simple MIP JuMP Model")
+
+    # Set inputs
+    set_interface_object(model, "lower_bound", 1.2)
+
+    # Show info
+    @test get_name(model) == "Simple MIP JuMP Model"
+    @test get_system(model) == "jump"
+    @test get_status(model) == "created"
+
+    # Solve
+    MOSInterface.run(model)
+
+    # Status
+    @test get_status(model) == "success"
+
+end    
